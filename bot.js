194b4bb82e8832debca6300431e1748623348b55
rockisthe <53100795+rockisthe@users.noreply.github.com>
2019-07-20 01:27:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,82 +1,27 @@
-var Discord = require("discord.js");
-var prefix = "t!";
-var client = new Discord.Client();
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import random
+from discord import Game
 
-client.on("ready", () => {
-  console.log("ready to rumble!");
-});
 
-var bannedwords = "fuck,shit,slut,whore".split(",");
+Client = discord.client
+client = commands.Bot(command_prefix = '!')
+Clientdiscord = discord.Client()
 
-client.on("message", msg => {
-  if (msg.guild === null) return;
 
-  for (i=0;i<bannedwords.length;i++) {
-    if (msg.content.toLowerCase().includes(bannedwords[i])) {
-      msg.delete();
-      msg.reply("Please don't swear!");
-      return;
-    }
-  }
+@client.event
+async def on_ready():
+    await client.change_presence(game=Game(name='playing roblox'))
+    print('Ready, Freddy') 
 
-  if (msg.author.bot) return;
-  if (!msg.member.hasPermission("ADMINISTRATOR")) return;
 
-  if (!msg.content.toLowerCase().startsWith(prefix)) return;
-  msg.delete();
-  if (msg.content.toLowerCase().startsWith(prefix + "kick ")) {
-    var mem = msg.mentions.members.first();
-    mem.kick().then(() => {
-      msg.channel.send(mem.displayName + " has successfully been kicked by " + msg.author.username + "!");
-    }).catch(e => {
-      msg.channel.send("An error occured!");
-    });
-  }
-  if (msg.content.toLowerCase().startsWith(prefix + "ban ")) {
-    var mem = msg.mentions.members.first();
-    var mc = msg.content.split(" ")[2];
-    mem.ban(mc).then(() => {
-      msg.channel.send(mem.displayName + " has successfully been banned by " + msg.author.username + " for " + mc + " days!");
-    }).catch(e => {
-      msg.channel.send("An error occured!");
-    });
-  }
-  if (msg.content.toLowerCase().startsWith(prefix + "mute")) {
-    var mem = msg.mentions.members.first();
-    if (msg.guild.roles.find("name", "Muted")) {
-      mem.addRole(msg.guild.roles.find("name", "Muted")).then(() => {
-        msg.channel.send(mem.displayName + " has successfully been muted!");
-      }).catch(e => {
-        msg.channel.send("An error occured!");
-        console.log(e);
-      });
-
-    }
-  }
-  if (msg.content.toLowerCase().startsWith(prefix + "unmute")) {
-    var mem = msg.mentions.members.first();
-    if (msg.guild.roles.find("name", "Muted")) {
-      mem.removeRole(msg.guild.roles.find("name", "Muted")).then(() => {
-        msg.channel.send(mem.displayName + " has successfully been unmuted!");
-      }).catch(e => {
-        msg.channel.send("An error occured!");
-        console.log(e);
-      });
-
-    }
-  }
-  if (msg.content.toLowerCase().startsWith(prefix + "purge")) {
-    var mc = msg.content.split(" ")[1];
-    msg.channel.bulkDelete(mc);
-  }
-  if (msg.content.toLowerCase().startsWith(prefix + "eval")) {
-    var sc = msg.content.substring(msg.content.indexOf(" "));
-    eval(sc);
-  }
-  if (msg.content.toLowerCase().startsWith(prefix + "calc")) {
-    var ca = msg.content.substring(msg.content.indexOf(" "));
-    msg.reply(ca + " is " + eval(ca).toFixed(2));
-  }
-});
-
-client.login(Your token here!);
+@client.event
+async def on_message(message):
+    if message.content == '!shit':
+        await client.send_message(message.channel,'get a fucking life')
+    if ('gay') in message.content:
+       await client.delete_message(message)
+client.run('NjAxOTA2NDI0Mjc2MTg5MjA1.XTJNvw.B6KRdhdjUDu7o3IhWbDs1wawN4M')
